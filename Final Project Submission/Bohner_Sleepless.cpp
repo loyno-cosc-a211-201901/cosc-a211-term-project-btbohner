ce1bb3388c21f73cd9ed0ae2c022bcd24e8ba97b
Blake Bohner <btbohner@my.loyno.edu>
2019-03-27 09:17:38 -0500

    Update Bohner_Sleepless.cpp
    Added cout statements to output title art.

--- Final Project Submission/Bohner_Sleepless.cpp
+++ Final Project Submission/Bohner_Sleepless.cpp
@@ -1,28 +1,36 @@
-cout << "############################################################################################################################################################"
-cout << "############################################################################################################################################################"
-cout << "##                                                                                                                                                        ##"
-cout << "##     SSSSSSSSSSSSSSS lllllll                                                             lllllll                                                        ##"
-cout << "##   SS:::::::::::::::Sl:::::l                                                             l:::::l                                                        ##"
-cout << "##  S:::::SSSSSS::::::Sl:::::l                                                             l:::::l                                                        ##"
-cout << "##  S:::::S     SSSSSSSl:::::l                                                             l:::::l                                                        ##"
-cout << "##  S:::::S             l::::l     eeeeeeeeeeee        eeeeeeeeeeee    ppppp   ppppppppp    l::::l     eeeeeeeeeeee        ssssssssss       ssssssssss    ##"
-cout << "##  S:::::S             l::::l   ee::::::::::::ee    ee::::::::::::ee  p::::ppp:::::::::p   l::::l   ee::::::::::::ee    ss::::::::::s    ss::::::::::s   ##"
-cout << "##   S::::SSSS          l::::l  e::::::eeeee:::::ee e::::::eeeee:::::eep:::::::::::::::::p  l::::l  e::::::eeeee:::::eess:::::::::::::s ss:::::::::::::s  ##"
-cout << "##    SS::::::SSSSS     l::::l e::::::e     e:::::ee::::::e     e:::::epp::::::ppppp::::::p l::::l e::::::e     e:::::es::::::ssss:::::ss::::::ssss:::::s ##"
-cout << "##      SSS::::::::SS   l::::l e:::::::eeeee::::::ee:::::::eeeee::::::e p:::::p     p:::::p l::::l e:::::::eeeee::::::e s:::::s  ssssss  s:::::s  ssssss  ##"
-cout << "##         SSSSSS::::S  l::::l e:::::::::::::::::e e:::::::::::::::::e  p:::::p     p:::::p l::::l e:::::::::::::::::e    s::::::s         s::::::s       ##"
-cout << "##              S:::::S l::::l e::::::eeeeeeeeeee  e::::::eeeeeeeeeee   p:::::p     p:::::p l::::l e::::::eeeeeeeeeee        s::::::s         s::::::s    ##"
-cout << "##              S:::::S l::::l e:::::::e           e:::::::e            p:::::p    p::::::p l::::l e:::::::e           ssssss   s:::::s ssssss   s:::::s  ##"
-cout << "##  SSSSSSS     S:::::Sl::::::le::::::::e          e::::::::e           p:::::ppppp:::::::pl::::::le::::::::e          s:::::ssss::::::ss:::::ssss::::::s ##"
-cout << "##  S::::::SSSSSS:::::Sl::::::l e::::::::eeeeeeee   e::::::::eeeeeeee   p::::::::::::::::p l::::::l e::::::::eeeeeeee  s::::::::::::::s s::::::::::::::s  ##"
-cout << "##  S:::::::::::::::SS l::::::l  ee:::::::::::::e    ee:::::::::::::e   p::::::::::::::pp  l::::::l  ee:::::::::::::e   s:::::::::::ss   s:::::::::::ss   ##"
-cout << "##   SSSSSSSSSSSSSSS   llllllll    eeeeeeeeeeeeee      eeeeeeeeeeeeee   p::::::pppppppp    llllllll    eeeeeeeeeeeeee    sssssssssss      sssssssssss     ##"
-cout << "##                                                                      p:::::p                                                                           ##"
-cout << "##                                                                      p:::::p                                                                           ##"
-cout << "##                                                                     p:::::::p                                                                          ##"
-cout << "##                                                                     p:::::::p                                                                          ##"
-cout << "##                                                                     p:::::::p                                                                          ##"
-cout << "##                                                                     ppppppppp                                                                          ##"
-cout << "##                                                                                                                                                        ##"
-cout << "############################################################################################################################################################"
-cout << "############################################################################################################################################################"
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+cout << "\n";
+cout << "############################################################################################################################################################\n";
+cout << "############################################################################################################################################################\n";
+cout << "##                                                                                                                                                        ##\n";
+cout << "##     SSSSSSSSSSSSSSS lllllll                                                             lllllll                                                        ##\n";
+cout << "##   SS:::::::::::::::Sl:::::l                                                             l:::::l                                                        ##\n";
+cout << "##  S:::::SSSSSS::::::Sl:::::l                                                             l:::::l                                                        ##\n";
+cout << "##  S:::::S     SSSSSSSl:::::l                                                             l:::::l                                                        ##\n";
+cout << "##  S:::::S             l::::l     eeeeeeeeeeee        eeeeeeeeeeee    ppppp   ppppppppp    l::::l     eeeeeeeeeeee        ssssssssss       ssssssssss    ##\n";
+cout << "##  S:::::S             l::::l   ee::::::::::::ee    ee::::::::::::ee  p::::ppp:::::::::p   l::::l   ee::::::::::::ee    ss::::::::::s    ss::::::::::s   ##\n";
+cout << "##   S::::SSSS          l::::l  e::::::eeeee:::::ee e::::::eeeee:::::eep:::::::::::::::::p  l::::l  e::::::eeeee:::::eess:::::::::::::s ss:::::::::::::s  ##\n";
+cout << "##    SS::::::SSSSS     l::::l e::::::e     e:::::ee::::::e     e:::::epp::::::ppppp::::::p l::::l e::::::e     e:::::es::::::ssss:::::ss::::::ssss:::::s ##\n";
+cout << "##      SSS::::::::SS   l::::l e:::::::eeeee::::::ee:::::::eeeee::::::e p:::::p     p:::::p l::::l e:::::::eeeee::::::e s:::::s  ssssss  s:::::s  ssssss  ##\n";
+cout << "##         SSSSSS::::S  l::::l e:::::::::::::::::e e:::::::::::::::::e  p:::::p     p:::::p l::::l e:::::::::::::::::e    s::::::s         s::::::s       ##\n";
+cout << "##              S:::::S l::::l e::::::eeeeeeeeeee  e::::::eeeeeeeeeee   p:::::p     p:::::p l::::l e::::::eeeeeeeeeee        s::::::s         s::::::s    ##\n";
+cout << "##              S:::::S l::::l e:::::::e           e:::::::e            p:::::p    p::::::p l::::l e:::::::e           ssssss   s:::::s ssssss   s:::::s  ##\n";
+cout << "##  SSSSSSS     S:::::Sl::::::le::::::::e          e::::::::e           p:::::ppppp:::::::pl::::::le::::::::e          s:::::ssss::::::ss:::::ssss::::::s ##\n";
+cout << "##  S::::::SSSSSS:::::Sl::::::l e::::::::eeeeeeee   e::::::::eeeeeeee   p::::::::::::::::p l::::::l e::::::::eeeeeeee  s::::::::::::::s s::::::::::::::s  ##\n";
+cout << "##  S:::::::::::::::SS l::::::l  ee:::::::::::::e    ee:::::::::::::e   p::::::::::::::pp  l::::::l  ee:::::::::::::e   s:::::::::::ss   s:::::::::::ss   ##\n";
+cout << "##   SSSSSSSSSSSSSSS   llllllll    eeeeeeeeeeeeee      eeeeeeeeeeeeee   p::::::pppppppp    llllllll    eeeeeeeeeeeeee    sssssssssss      sssssssssss     ##\n";
+cout << "##                                                                      p:::::p                                                                           ##\n";
+cout << "##                                                                      p:::::p                                                                           ##\n";
+cout << "##                                                                     p:::::::p                                                                          ##\n";
+cout << "##                                                                     p:::::::p                                                                          ##\n";
+cout << "##                                                                     p:::::::p                                                                          ##\n";
+cout << "##                                                                     ppppppppp                                                                          ##\n";
+cout << "##                                                                                                                                                        ##\n";
+cout << "############################################################################################################################################################\n";
+cout << "############################################################################################################################################################\n";
+}
